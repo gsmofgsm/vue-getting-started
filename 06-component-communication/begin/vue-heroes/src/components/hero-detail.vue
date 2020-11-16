b40942c9effb0b41ffa795b3ad518f5910345d03
Qing Ma <gsmofgsm@hotmail.com>
2020-11-16 19:32:00 +0100

    nest HeroDetail in Heroes

---
+++ 06-component-communication/begin/vue-heroes/src/components/hero-detail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Hello!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero detail',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- 06-component-communication/begin/vue-heroes/src/components/heroes.vue
+++ 06-component-communication/begin/vue-heroes/src/components/heroes.vue
@@ -31,6 +31,7 @@
           <!-- <heroes-list> -->
 
           <!-- <hero-detail> -->
+          <HeroDetail v-if="selectedHero" />
           <div v-if="selectedHero">
             <div class="card">
               <header class="card-header">
@@ -127,6 +128,8 @@ import { format } from 'date-fns';
 
 import { displayDateFormat, ourHeroes } from '../shared';
 
+import HeroDetail from '@/components/hero-detail';
+
 export default {
   name: 'Heroes',
   data() {
@@ -137,6 +140,7 @@ export default {
       capeMessage: '',
     };
   },
+  components: { HeroDetail },
   created() {
     this.loadHeroes();
   },
