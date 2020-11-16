c31b63362810470be92853f77f1d1610cd56dfd4
Qing Ma <gsmofgsm@hotmail.com>
2020-11-16 15:18:28 +0100

    two-way binding with v-model

--- 03-data-and-events/begin/vue-heroes/src/components/heroes.vue
+++ 03-data-and-events/begin/vue-heroes/src/components/heroes.vue
@@ -17,28 +17,48 @@
               </div>
               <div class="field">
                 <label class="label" for="firstName">first name</label>
-                <input class="input" id="firstName" />
+                <input class="input" id="firstName" v-model="hero.firstName" />
               </div>
               <div class="field">
                 <label class="label" for="lastName">last name</label>
-                <input class="input" id="lastName" />
+                <input class="input" id="lastName" v-model="hero.lastName" />
               </div>
               <div class="field">
                 <label class="label" for="description">description</label>
-                <textarea class="input" id="description" type="text" />
+                <textarea
+                  class="input"
+                  id="description"
+                  type="text"
+                  v-model="hero.description"
+                />
               </div>
               <div class="field">
                 <label class="label">cape color</label>
                 <label class="radio" for="color-red">
-                  <input type="radio" id="color-red" value="red" />
+                  <input
+                    type="radio"
+                    id="color-red"
+                    value="red"
+                    v-model="hero.capeColor"
+                  />
                   red
                 </label>
                 <label class="radio" for="color-blue">
-                  <input type="radio" id="color-blue" value="blue" />
+                  <input
+                    type="radio"
+                    id="color-blue"
+                    value="blue"
+                    v-model="hero.capeColor"
+                  />
                   blue
                 </label>
                 <label class="radio" for="color-green">
-                  <input type="radio" id="color-green" value="green" />
+                  <input
+                    type="radio"
+                    id="color-green"
+                    value="green"
+                    v-model="hero.capeColor"
+                  />
                   green
                 </label>
                 <div class="color-line"></div>
@@ -47,7 +67,7 @@
                 <label for="power">
                   super power
                   <div class="select is-primary">
-                    <select id="power">
+                    <select id="power" v-model="hero.power">
                       <option disabled value>Please select one</option>
                       <option>Speed</option>
                       <option>Flight</option>
@@ -60,7 +80,12 @@
               <div class="field">
                 <label class="checkbox" for="active">
                   active
-                  <input type="checkbox" class="is-primary" id="active" />
+                  <input
+                    type="checkbox"
+                    class="is-primary"
+                    id="active"
+                    v-model="hero.active"
+                  />
                 </label>
               </div>
             </div>
